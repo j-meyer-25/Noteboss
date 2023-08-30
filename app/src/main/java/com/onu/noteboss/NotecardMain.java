e74b597aa10b5561b116b32dd8c6afc05955c14d
j-meyer-25 <j-meyer.13@onu.edu>
2023-08-30 12:00:16 -0400

    test!

---
+++ app/src/main/java/com/onu/noteboss/NotecardMain.java
@@ -0,0 +1,14 @@
+package com.onu.noteboss;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+// main
+public class NotecardMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
